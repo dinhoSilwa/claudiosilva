75e13fbd5a8b0566aba01a8ec0307d97d035ab48
dinhoSilwa <dinhosilwa_@hotmail.com>
2024-06-23 00:59:04 -0300

    style:(Hero) change background color for any compoennets

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export const Hero = () => {
       <section className="bg-hero flex flex-col justify-start gap-[32px] items-center pt-20 bg-zinc-200 h-auto xs:pt-20 xs:pb-20 xxs:gap-[40px]">
         <img
           src={frontEndGif}
-          className="z-20 w-[50%] h-auto sm:w-[60%] md:hidden lg:[30%]"
+          className="w-[50%] h-auto sm:w-[60%] md:hidden lg:[30%]"
         />
         <h2 className="xs:text-[36px] md:text-[44px] leading-none md:leading-[56px] font-source font-extrabold text-[28px] flex items-center text-center justify-center text-zinc-950 tracking-wider">
           OLÁ, EU SOU O <br></br>CLAUDIO SILVA
